51352636d2a196a6d2e3e168d4181967ae007911
Diana Portillo <diana.portillo01@gmail.com>
2022-08-02 19:31:16 -0700

    starter page

---
+++ seeds/users.js
@@ -0,0 +1,14 @@
+module.exports - [
+    {
+        username: 'dianap',
+        password: 'password2'
+    },
+    {
+        username: 'dianap1',
+        password: 'password3'
+    },
+    {
+        username: 'dianap2',
+        password: 'password4'
+    },
+];
---
+++ server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const expsesh = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(expsesh.Store);
+const sequelize = require('./config/connection');
+const routes = require('./controllers/homepageController');
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({
+   helpers,
+});
+
+const sessionSettings = {
+   secret: process.env.SESSION_SECRET,
+   resave: false,
+
+   saveUnitialized: false,
+   store: new SequelizeStore({
+      db: sequelize,
+   }),
+};
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3302;
+
+// template engine setup
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+ app.use(expsesh(sessionSettings));
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+   app.listen(PORT, () => console.log('WE MADE IT'));
+});
