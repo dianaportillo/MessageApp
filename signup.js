c1cda8e1b6f0b471c49ea2c4f3bb77fb9eaedffb
NaShari <104246897+NaShari@users.noreply.github.com>
2022-08-09 20:18:58 -0700

    Add files via upload

--- signup.js
+++ signup.js
@@ -1,53 +1,39 @@
 const signupBtn = document.getElementById('signupBtn');
-const signupUsernameInput = document.getElementById('signupUsernameInput');
-const signupPasswordInput = document.getElementById('signupPasswordInput');
-const signupRepeatPasswordInput = document.getElementById('signupRepeatPasswordInput');
-const signupEmailInput = document.getElementById('signupEmailInput');
+const email = document.getElementById('signupEmailInput');
+const password = document.getElementById('signuppasswordInput');
+const repeatpassword = document.getElementById('signuprepeatpassword');
+const Username = document.getElementById('signupUsername');
 
 signupBtn.addEventListener('click', async (event) => {
-    event.preventDefault();
-    const email = signupEmailInput.value;
-    const password = signupPasswordInput.value;
-    const repeatPassword = signupRepeatPasswordInput.value;
-    const username = signupUsernameInput.value;
-
-    // checks that email is enterd 
-    if (email.trim().length === 0) {
-        alert('please enter valid email')
-    }
-    // checks to make sure username is not empty
-    if (username.trim().length === 0) {
-        alert('Please enter a valid username');
-        return;
-    }
-    // checks that password is greater than 6 characters
-    if (password.trim().length < 6) {
-        alert('Please enter a valid password. Password must be 6 characters long.');
-        return;
-    }
-    // checks that repeat password is the same as password
-    if (repeatPassword !== password) {
-        alert('your passwords do not match');
-        return;
-    }
-
-    // posts the user input to the /api/signup endpoint
-    try {
-        const response = await fetch('/api/signin', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username,
-                password,
-            })
-        });
-
-        await response.json();
-        // change user window to the /users endpoint
-        window.location.href = '/todos';
-    } catch (error) {
-        alert(error);
-    }
+  event.preventDefault();
+  // checks to make sure username is not empty
+  if (username.trim().length === 0) {
+    alert('Please enter a valid username');
+    return;
+  }
+  // checks that password is greater than 6 characters
+  if (password.trim().length < 6) {
+    alert('Please enter a valid password. Password must be 6 characters long.');
+    return;
+  }
+  // checks that repeatpassword=password 
+  if (repeatpassword !== password) {
+    alert('your password does not equal your previous password');
+    return;
+  }
 });
+//functional email
+function EmailValidation(enteredEmail) {
+  var mail_format = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+
+  if (enteredEmail.value.match(enteredEmail)) {
+    document.form1.text1.focus();
+    return true;
+  }
+  Else
+  {
+    alert('sorry your email was invalid');
+    document.form1.text1.focus();
+    return false;
+  }
+}
